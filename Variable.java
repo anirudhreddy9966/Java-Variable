c62d89b057a5e1612326cb33918d649062de5032
anirudhreddy9966 <139291438+anirudhreddy9966@users.noreply.github.com>
2024-04-02 03:47:16 -0500

    Add files via upload

---
+++ Variable.java
@@ -0,0 +1,15 @@
+
+import java.lang.*;
+
+public class Variable
+{
+   public static void main(String args[])
+   {
+     byte b=5;
+
+       b=25;
+     System.out.println(b);
+
+
+   }
+}
---
+++ Variables.java
@@ -0,0 +1,14 @@
+
+import java.lang.*;
+
+public class Variables
+{
+   public static void main(String args[])
+   {
+    int a;
+    int roll_number;
+    int averagenumberofstudents;
+
+
+   }
+}
